142ddee9586b177f51ca21b419fb492e2c88ea86
adrsy6394 <aky386832@gmail.com>
2026-05-10 18:54:38 +0530

    fix: definitively removed server directory and moved backend to root

--- server/src/routes/syllabusRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getSyllabus } from '../controller/syllabusController.js';
-
-const router = express.Router();
-
-router.get('/', getSyllabus);
-
-export default router;
